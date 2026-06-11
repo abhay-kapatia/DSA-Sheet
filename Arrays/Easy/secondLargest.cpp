3c8ce9141233f59b5f9049f3faa52227e3c4ffc6
Abhay Kapatia <78949998+abhay-kapatia@users.noreply.github.com>
2026-06-12 02:02:13 +0530

    2nd largest element

--- Arrays/Easy/secondLargest.cpp
+++ Arrays/Easy/secondLargest.cpp
@@ -2,16 +2,20 @@ class Solution {
   public:
     int getSecondLargest(vector<int> &arr) {
         // code here
-        int l = INT_MIN;
-        int sl = INT_MIN;
-        for(int i = 0; i < arr.size(); i++){
-            if(l<arr[i]){
-                sl = l;
-                l = arr[i];
-            }else if(arr[i] < l && sl < arr[i]){
-                sl = arr[i];
+        int largest = INT_MIN;
+        int secondLargest = INT_MIN;
+        // these should be INT_MIN just to be safe as if arr is [10,5,10] then it will cause problem
+        for(int i=0; i<arr.size();i++){
+            if(arr[i]>largest){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            // in second check, strictly it shouldn't be equal to largest 
+            if(arr[i]>secondLargest && arr[i]!=largest){
+                secondLargest = arr[i];
             }
         }
-        return sl;
+        // if second largest doesn't exist it should be -1 as in [10,10,10]
+        return (secondLargest == INT_MIN) ? -1 : secondLargest;
     }
 };
