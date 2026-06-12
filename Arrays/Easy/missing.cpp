8552fb89d39902f7aa931d4efd642608c3953c06
Abhay Kapatia <78949998+abhay-kapatia@users.noreply.github.com>
2026-06-12 16:01:06 +0530

    Add two approaches to find missing number in array
    Implemented the first approach using a mathematical formula to find the missing number. Started the second approach using XOR for the same functionality.

--- Arrays/Easy/missing.cpp
+++ Arrays/Easy/missing.cpp
@@ -1,3 +1,18 @@
+// 1st approach using mathematical formulae
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+        int n = arr.size();
+        long long sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        long long expected = (long long)(n + 1) * (n + 2) / 2;
+        return (int)(expected - sum);
+    }
+};
+
+//2nd approach using XOR
 class Solution {
   public:
     int missingNum(vector<int>& arr) {
