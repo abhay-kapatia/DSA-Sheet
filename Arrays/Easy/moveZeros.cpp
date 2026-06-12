1ad0c829757fe3bd50b22c3635c5c8d9b07170b1
Abhay Kapatia <78949998+abhay-kapatia@users.noreply.github.com>
2026-06-12 14:47:36 +0530

    move zero to the end
    Refactor moveZeroes function to improve readability and logic.

--- Arrays/Easy/moveZeros.cpp
+++ Arrays/Easy/moveZeros.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& arr) {
-        int p = -1;
-        for(int i = 0; i<arr.size(); i++){
-            if(arr[i]==0){
-                p = i;
+    void moveZeroes(vector<int>& nums) {
+        int j =-1; // initialization + to find the case where there is no zero;
+        // find the first occurance of zero
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0){
+                j = i;
                 break;
             }
         }
-        if(p==-1) return;
-        for(int i=p+1; i<arr.size(); i++){
-            if(arr[i]!=0){
-                swap(arr[i],arr[p]);
-                p++;
+        // if no zero just return
+        if (j==-1) return;
+        // place the 2nd pointer just next toi the zero one
+        // then keeping on swapping
+        for(int i = j+1; i<nums.size(); i++){
+            if(nums[i]!=0){
+                swap(nums[i], nums[j]);
+                j++;
             }
-        }   
+        }
     }
 };
