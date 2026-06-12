4676c37f774620b073e07d64a87e2eded13146ad
Abhay Kapatia <78949998+abhay-kapatia@users.noreply.github.com>
2026-06-12 11:29:29 +0530

    is sorted or sorted when  rotated a few times

--- Arrays/Easy/isSorted.cpp
+++ Arrays/Easy/isSorted.cpp
@@ -1,12 +1,16 @@
 class Solution {
-  public:
-    bool isSorted(vector<int>& arr) {
-        // code here
-        for(int i=1; i < arr.size(); i++){
-            if(arr[i-1]>arr[i]){
-                return false;
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        // count is to know the break point in array 
+        // the main goal is to know the break point circularly
+        for(int i=0 ; i<nums.size();i++){
+            // Count places where the sorted order breaks (including last element to first)
+            if(nums[i]>nums[(i+1)%n]){
+                count++;
             }
         }
-        return true;
+         return (count > 1)? false : true;
     }
 };
