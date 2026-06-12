ff2e4f5d1f7b5e4e6adbad3b08bd9ce3f744922a
Abhay Kapatia <78949998+abhay-kapatia@users.noreply.github.com>
2026-06-12 13:14:06 +0530

    remove duplicate from sorted element

--- Arrays/Easy/removeDuplicate2.cpp
+++ Arrays/Easy/removeDuplicate2.cpp
@@ -1,28 +1,19 @@
-//80. Remove Duplicates from Sorted Array II
-
-// Example 1:
-// Input: nums = [1,1,1,2,2,3]
-// Output: 5, nums = [1,1,2,2,3,_]
-// Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
-// It does not matter what you leave beyond the returned k (hence they are underscores).
-
+// 1st approach is take mapping in a map and then count the map 
+// 2nd approach is as the element is sorted take 2 pointer approach
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        map<int, int> mp;
-        for (int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
-        }
-        int j = 0;
-        for(auto i : mp){
-            if(i.second==1){
-                nums[j++] = i.first;
-            }
-            else if(i.second>=2){
-                nums[j++] = i.first;
-                nums[j++] = i.first;
-            }
-        }
-        return j;
+             int count = 1;
+             // as array is sorted, there is a scop of 2 pointer approach
+             for(int j=1, i=0; j<nums.size(); j++){
+                // just to check that the element is not equal
+                if(nums[j] != nums[i]){
+                    // replace the duplicate with unique element
+                    nums[i+1] = nums[j];
+                    count++;
+                    i++;
+                }
+             }
+             return count;
     }
 };
